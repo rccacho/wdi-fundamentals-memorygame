ed54506016087d41226826fc034bef4215118b7b
rccacho <rccacho@gmail.com>
2017-01-21 14:20:28 -0800

    javascript to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,8 +8,19 @@ var cardThree = "king";
 
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-  } else {
-  	alert("Sorry, try again.");
+// if (cardOne === cardTwo) {
+// 	alert("You found a match!");
+//   } else {
+//   	alert("Sorry, try again.");
+//   }
+
+var gameboard = document.getElementById('game-board');
+
+var createCards = function (num) {
+  for (var i = 0; i < num; i++) {
+    var createDiv = document.createElement('div');
+    createDiv.className = 'card'; 
+    document.getElementById('game-board').appendChild(createDiv);
   }
+}
+createCards(4)
